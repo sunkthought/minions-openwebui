cf0201d4a307d3a3d8221c589e8a21bc8fc3bbde
sunkthought <cinem4rl@gmail.com>
2025-05-30 22:45:32 -0400

    boosh

--- generated_functions/minion_default_function.py
+++ generated_functions/minion_default_function.py
@@ -5,7 +5,7 @@ author_url: https://github.com/SunkThought/minions-openwebui
 original_author: Copyright (c) 2025 Sabri Eyuboglu, Avanika Narayan, Dan Biderman, and the rest of the Minions team (@HazyResearch wrote the original MinionS Protocol paper and code examples on github that spawned this)
 original_author_url: https://github.com/HazyResearch/
 funding_url: https://github.com/HazyResearch/minions
-version: 0.2.2
+version: 0.2.6
 description: Basic Minion protocol - conversational collaboration between local and cloud models
 required_open_webui_version: 0.5.0
 license: MIT License
